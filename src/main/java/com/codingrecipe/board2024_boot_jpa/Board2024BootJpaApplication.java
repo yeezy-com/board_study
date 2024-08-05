23659956cd0f297c1886fa51506e04b20d572333
GiSeok Kang <ime.rokz@gmail.com>
2024-08-06 02:10:13 +0900

    First Commit

---
+++ src/main/java/com/codingrecipe/board2024_boot_jpa/Board2024BootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.codingrecipe.board2024_boot_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board2024BootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Board2024BootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingrecipe/board2024_boot_jpa/Board2024BootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingrecipe.board2024_boot_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Board2024BootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
