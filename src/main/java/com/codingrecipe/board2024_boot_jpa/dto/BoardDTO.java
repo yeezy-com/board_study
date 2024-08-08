015c148590dad0c0e5305876d9f32ba41750f80a
GiSeok Kang <ime.rokz@gmail.com>
2024-08-09 00:40:41 +0900

    09_파일첨부 2

--- src/main/java/com/codingrecipe/board2024_boot_jpa/dto/BoardDTO.java
+++ src/main/java/com/codingrecipe/board2024_boot_jpa/dto/BoardDTO.java
@@ -1,6 +1,7 @@
 package com.codingrecipe.board2024_boot_jpa.dto;
 
 import com.codingrecipe.board2024_boot_jpa.entity.BoardEntity;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 
@@ -16,7 +17,12 @@ public record BoardDTO(
         String boardContents,
         int boardHits,
         LocalDateTime boardCreatedTime,
-        LocalDateTime boardUpdatedTime
+        LocalDateTime boardUpdatedTime,
+        MultipartFile boardFile, // save.html -> Controller로 넘어올 때 파일 담는 객체
+        String originalFileName, // 원본 파일 이름
+        String storedFileName,   // 서버 저장용 파일 이름
+        // 서버 저장용 이름을 따로 두는 이유는 원본 파일 이름이 중복될 수 있기 때문이다.
+        int fileAttached         // 파일 첨부 여부(첨부1, 미첨부0)
 ) {
 //    public BoardDTO(Long id, String boardWriter, String boardTitle, int boardHits, LocalDateTime boardCreatedTime) {
 //        this(id, boardWriter, "", boardTitle, "", boardHits, boardCreatedTime, null);
@@ -25,7 +31,8 @@ public record BoardDTO(
     public static BoardDTO toBoardDTO(BoardEntity boardEntity) {
         BoardDTO boardDTO = new BoardDTO(boardEntity.getId(), boardEntity.getBoardWriter(),
                 boardEntity.getBoardPass(), boardEntity.getBoardTitle(), boardEntity.getBoardContents(),
-                boardEntity.getBoardHits(), boardEntity.getCreatedTime(), boardEntity.getUpdatedTime());
+                boardEntity.getBoardHits(), boardEntity.getCreatedTime(), boardEntity.getUpdatedTime(),
+                null, null, null, 0);
 
         return boardDTO;
     }
